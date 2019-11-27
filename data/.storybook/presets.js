b6944a95279f45811e4b92ef961496a20898926a
Yavuz Ege Özcan <egecan@gmail.com>
2019-11-27 18:43:26 +0100

    storybook

---
+++ data/.storybook/presets.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = [
+    {
+        name: '@storybook/preset-typescript',
+        options: {
+            tsLoaderOptions: {
+                configFile: path.resolve(__dirname, '../tsconfig.json'),
+            },
+            tsDocgenLoaderOptions: {
+                tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+            },
+            include: [path.resolve(__dirname, '../src')],
+        },
+    },
+];
