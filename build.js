6a8d0a68bb4dfafccf076996055228355221774c
Yavuz Ege Özcan <yavuz.oezcan@check24.de>
2020-02-24 08:50:33 +0100

    stdio inherit

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ const join = require("path").join;
 execSync("npm i", {
   windowsHide: true,
   cwd: join(__dirname, "./data"),
+  stdio: "inherit",
   env: {
     ...process.env,
     NODE_ENV: process.argv[2] || "development"
@@ -15,6 +16,7 @@ console.log("done installing modules");
 execSync("npx webpack", {
   windowsHide: true,
   cwd: join(__dirname, "./data"),
+  stdio: "inherit",
   env: {
     ...process.env,
     NODE_ENV: process.argv[2] || "development"
@@ -25,14 +27,16 @@ console.log("webpack build done");
 
 execSync('go-bindata-assetfs -prefix "data/"  data/dist/...', {
   windowsHide: true,
-  cwd: __dirname
+  cwd: __dirname,
+  stdio: "inherit"
 });
 
 console.log("bindata done");
 
 execSync("go build", {
   windowsHide: true,
-  cwd: __dirname
+  cwd: __dirname,
+  stdio: "inherit"
 });
 
 console.log("build done");
